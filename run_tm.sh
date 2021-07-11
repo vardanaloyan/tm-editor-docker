23ac161671da94feb2950e74dddaa45b0380feb9
Vardan <31190485+vardanaloyan@users.noreply.github.com>
2021-07-11 16:59:52 +0400

    Add files via upload

---
+++ run_tm.sh
@@ -0,0 +1,9 @@
+# Take the ip address
+ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+
+# Add ip address to the whitelist
+xhost + $ip
+
+# Run the docker image
+docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd):/opt/data -e DISPLAY=$ip:0 aloyan/tm-editor
+ 
