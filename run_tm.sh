afb59a4851a069fdbaa81e0e0223c93d0f190622
Vardan <valoyan.contractor@libertyglobal.com>
2021-11-25 01:38:16 +0400

    fixing version in sh

--- run_tm.sh
+++ run_tm.sh
@@ -5,5 +5,5 @@ ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $ip
 
 # Run the docker image
-docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd):/opt/data -e DISPLAY=$ip:0 aloyan/tm-editor:0.12.1
+docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd):/opt/data -e DISPLAY=$ip:0 aloyan/tm-editor:latest
  
